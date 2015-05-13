3dfaaed583ef897e283a0221f408e71c8e402586
tennfy <admin@tennfy.com>
2015-05-13 23:00:13 +0800

    change eof 2

--- debian_lnmp_tennfy.sh
+++ debian_lnmp_tennfy.sh
@@ -235,9 +235,11 @@ function addvirtualhost(){
 	location ~ \.php$ {
 		fastcgi_split_path_info ^(.+\.php)(/.+)$;
         fastcgi_pass unix:/var/run/php5-fpm.sock;
-        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
         fastcgi_index index.php;
         include fastcgi_params;
+EOF
+cat  >> /etc/nginx/conf.d/${hostname}.conf <<"EOF"      
+        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
 	}
 	}
 EOF
