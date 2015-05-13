9c3c9a4c05136971bac0604e97862a4d750f3be4
tennfy <admin@tennfy.com>
2015-05-13 22:56:42 +0800

    change eof

--- debian_lnmp_tennfy.sh
+++ debian_lnmp_tennfy.sh
@@ -128,9 +128,11 @@ cat > /etc/nginx/sites-available/default <<EOF
 	fastcgi_split_path_info ^(.+\.php)(/.+)$;
 	# With php5-fpm:
 	fastcgi_pass unix:/var/run/php5-fpm.sock;
-	fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
 	fastcgi_index index.php;
 	include fastcgi_params;
+EOF	
+cat >> /etc/nginx/sites-available/default <<"EOF"	
+	fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;	
 	}
 	}
 EOF
